05f7003b9850725219d94da13a007672505661bb
sssi111 <65550209+sssi111@users.noreply.github.com>
2024-02-09 00:28:55 +0300

    Add dependencies (#2)
    * add dependencies (do not merge)
    
    * fix dependencies
    
    * fix issues

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,17 +1,27 @@
 plugins {
     id("application")
     id("java")
+    id("com.google.protobuf") version "0.9.4"
 }
+
 
 group = "org.hse.moodactivities.backend"
 version = "0.1-DEV"
 
 repositories {
     mavenCentral()
+    google()
 }
 
 dependencies {
     implementation("org.jetbrains:annotations:16.0.2")
+    implementation("com.google.protobuf:protobuf-java:3.25.1")
+
+    implementation("dev.morphia.morphia:morphia-core:2.4.11")
+    implementation("org.mongodb:mongodb-driver-sync:4.11.1")
+    implementation("io.grpc:grpc-netty:${property("grpcVersion")}")
+    implementation("io.grpc:grpc-protobuf:${property("grpcVersion")}")
+    implementation("io.grpc:grpc-stub:${property("grpcVersion")}")
     testImplementation(platform("org.junit:junit-bom:5.7.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -34,4 +44,23 @@ tasks.jar {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.1"
+    }
+
+    plugins {
+        create("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:${property("grpcVersion")}"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                create("grpc")
+            }
+        }
+    }
 }
