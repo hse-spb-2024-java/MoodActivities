3568d5d71bf9bea4af7dad046abac7556fdab237
kristina driam <kristina.driam@gmail.com>
2024-02-21 00:24:40 +0300

    add home screen (#5)
    * feat frontend: add basic classes and layouts
    
    * feat frontend: change xml and add buttons to widgets
    
    * feat frontend: add week widget
    
    * feat frontend: add ask widget and fix layouts

---
+++ app/src/main/java/org/hse/moodactivities/InsightsScreen.kt
@@ -0,0 +1,16 @@
+package org.hse.moodactivities
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class InsightsScreen : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_insights_screen, container, false)
+    }
+}
