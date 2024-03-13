75119c98af97c1e92610834b8cfb7bfbaeca66ad
kristina <kvyakovleva_1@edu.hse.ru>
2024-03-13 07:29:21 +0300

    feat frontend: change charts library and add charts with week mood

--- app/src/main/java/org/hse/moodactivities/fragments/InsightsScreenFragment.kt
+++ app/src/main/java/org/hse/moodactivities/fragments/InsightsScreenFragment.kt
@@ -1,62 +1,25 @@
 package org.hse.moodactivities.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.charts.LineChart
 import org.hse.moodactivities.R
-import io.data2viz.color.Colors
-import io.data2viz.geom.size
-import io.data2viz.scale.Scales
-import io.data2viz.viz.*
+import org.hse.moodactivities.services.ChartsService
+
 
 class InsightsScreenFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-//        return inflater.inflate(R.layout.fragment_insights_screen, container, false)
-        return viz.toView(this.requireContext())
-    }
-}
+        val view = inflater.inflate(R.layout.fragment_insights_screen, container, false)
 
+        val lineChart = view.findViewById<LineChart>(R.id.line_chart)
+        ChartsService.createWeekMoodCharts(this.requireActivity().resources, lineChart)
 
-const val vizSize = 500.0
-
-val data = listOf(4, 8, 15, 16, 23, 42)
-const val barHeight = 14.0
-const val padding = 2.0
-
-
-val xScale = Scales.Continuous.linear {
-    domain = listOf(.0, data.max().toDouble())
-    range = listOf(.0, vizSize - 2 * padding)
-}
-
-val viz : Viz = viz {
-    size = size(vizSize, vizSize)
-    data.forEachIndexed { index, datum ->
-        group {
-            transform {
-                translate(
-                    x = padding,
-                    y = padding + index * (padding + barHeight))
-            }
-            rect {
-                width = xScale(datum)
-                height = barHeight
-                fill = Colors.Web.indianred
-            }
-            text {
-                textContent = datum.toString()
-                hAlign = TextHAlign.RIGHT
-                vAlign = TextVAlign.HANGING
-                x = xScale(datum) - 2.0
-                y = 1.5
-                textColor = Colors.Web.white
-                fontSize = 10.0
-            }
-        }
+        return view
     }
 }
