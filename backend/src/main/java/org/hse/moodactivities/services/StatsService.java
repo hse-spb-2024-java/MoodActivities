49e750c7a99c40455df2ff15792a14033f52d34a
sssi111 <gooday358@gmail.com>
2024-03-26 21:33:40 +0300

    fixes

--- backend/src/main/java/org/hse/moodactivities/services/StatsService.java
+++ backend/src/main/java/org/hse/moodactivities/services/StatsService.java
@@ -16,14 +16,17 @@ import org.hse.moodactivities.common.proto.responses.stats.WeeklyReportResponse;
 import org.hse.moodactivities.common.proto.services.StatsServiceGrpc;
 import org.hse.moodactivities.data.entities.mongodb.User;
 import org.hse.moodactivities.data.entities.mongodb.UserDayMeta;
+import org.hse.moodactivities.utils.JWTUtils.JWTUtils;
 import org.hse.moodactivities.utils.MongoDBSingleton;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import io.grpc.stub.StreamObserver;
 
@@ -31,7 +34,7 @@ public class StatsService extends StatsServiceGrpc.StatsServiceImplBase {
 
     private static User getUser(String userId) {
         Map<String, Object> queryMap = new HashMap<>();
-        queryMap.put("id", "123");
+        queryMap.put("id", userId);
         List<User> users = MongoDBSingleton.getInstance().getConnection().findEntityWithFilters(User.class, queryMap);
 
         if (users != null && !users.isEmpty()) {
@@ -41,13 +44,18 @@ public class StatsService extends StatsServiceGrpc.StatsServiceImplBase {
         }
     }
 
+    private static final int DAYS_IN_WEEK = 7;
+    private static final int DAYS_IN_MONTH = 31;
+    private static final int DAYS_IN_YEAR = 366;
+    private static final int DAYS_IN_UNIVERSE = Integer.MAX_VALUE;
+
     private static int periodToInt(PeriodType period) {
         int res = 0;
         switch (period) {
-            case WEEK -> res = 7;
-            case MONTH -> res = 31;
-            case YEAR -> res = 365;
-            case ALL -> res = Integer.MAX_VALUE;
+            case WEEK -> res = DAYS_IN_WEEK;
+            case MONTH -> res = DAYS_IN_MONTH;
+            case YEAR -> res = DAYS_IN_YEAR;
+            case ALL -> res = DAYS_IN_UNIVERSE;
         }
         return res;
     }
@@ -56,28 +64,43 @@ public class StatsService extends StatsServiceGrpc.StatsServiceImplBase {
         return ChronoUnit.DAYS.between(possibleDate, today) <= diff;
     }
 
+    private static List<UserDayMeta> getCorrectDaysSublist(List<UserDayMeta> metas, PeriodType period) {
+        int size = metas.size();
+        if (size >= periodToInt(period)) {
+            metas = metas.subList(size - periodToInt(period), size);
+        }
+        LocalDate today = LocalDate.now();
+        boolean isCorrect = false;
+        for (int i = 0; i < metas.size(); i++) {
+            if (isDateCorrect(metas.get(i).getDate(), today, periodToInt(period))) {
+                metas = metas.subList(i, metas.size());
+                isCorrect = true;
+                break;
+            }
+        }
+        return isCorrect ? metas : new ArrayList<>();
+    }
+
     @Override
     public void getFullReport(FullReportRequest request, StreamObserver<FullReportResponse> responseObserver) {
         int period = periodToInt(request.getPeriod());
         ReportType reportType = request.getReportType();
-        String userId = "123";
+        String userId = JWTUtils.CLIENT_ID_CONTEXT_KEY.get();
         User user = getUser(userId);
-        List<String> result;
+        Stream<List<String>> stream;
         if (reportType == ReportType.ACTIVITIES) {
-            result = user.getMetas().stream()
+            stream = getCorrectDaysSublist(user.getMetas(), request.getPeriod()).stream()
                     .map((item) -> item.getActivityList().stream()
-                            .map(activityItem -> activityItem.getType()).toList())
-                    .limit(period)
-                    .flatMap(List::stream)
-                    .collect(Collectors.toList());
+                            .map(activityItem -> activityItem.getType()).toList());
         } else {
-            result = user.getMetas().stream()
+            stream = getCorrectDaysSublist(user.getMetas(), request.getPeriod()).stream()
                     .map((item) -> item.getMoodList().stream()
-                            .map(moodItem -> moodItem.getType()).toList())
-                    .limit(period)
-                    .flatMap(List::stream)
-                    .collect(Collectors.toList());
+                            .map(moodItem -> moodItem.getType()).toList());
         }
+        List<String> result = stream.limit(period)
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+        ;
         FullReportResponse response = FullReportResponse.newBuilder().addAllReport(result).build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
@@ -87,44 +110,32 @@ public class StatsService extends StatsServiceGrpc.StatsServiceImplBase {
     public void getTopList(TopListRequest request, StreamObserver<TopListResponse> responseObserver) {
         int period = periodToInt(request.getPeriod());
         ReportType reportType = request.getReportType();
-        String userId = "123";
+        String userId = JWTUtils.CLIENT_ID_CONTEXT_KEY.get();
         User user = getUser(userId);
-        List<TopItem> result;
+        Stream<List<String>> stream;
         if (reportType == ReportType.ACTIVITIES) {
-            result = user.getMetas().stream()
+            stream = getCorrectDaysSublist(user.getMetas(), request.getPeriod()).stream()
                     .map((item) -> item.getActivityList().stream()
-                            .map(activityItem -> activityItem.getType()).toList())
-                    .flatMap(List::stream).
-                    collect(
-                            Collectors.groupingBy(
-                                    s -> s,
-                                    Collectors.counting()
-                            ))
-                    .entrySet().stream()
-                    .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
-                    .limit(3).map(entry -> TopItem.newBuilder()
-                            .setName(entry.getKey())
-                            .setAmount(Math.toIntExact(entry.getValue()))
-                            .build())
-                    .toList();
+                            .map(activityItem -> activityItem.getType()).toList());
         } else {
-            result = user.getMetas().stream()
+            stream = getCorrectDaysSublist(user.getMetas(), request.getPeriod()).stream()
                     .map((item) -> item.getMoodList().stream()
-                            .map(moodItem -> moodItem.getType()).toList())
-                    .flatMap(List::stream).
-                    collect(
-                            Collectors.groupingBy(
-                                    s -> s,
-                                    Collectors.counting()
-                            ))
-                    .entrySet().stream()
-                    .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
-                    .limit(3).map(entry -> TopItem.newBuilder()
-                            .setName(entry.getKey())
-                            .setAmount(Math.toIntExact(entry.getValue()))
-                            .build())
-                    .toList();
+                            .map(moodItem -> moodItem.getType()).toList());
         }
+        List<TopItem> result = stream
+                .flatMap(List::stream).
+                collect(
+                        Collectors.groupingBy(
+                                s -> s,
+                                Collectors.counting()
+                        ))
+                .entrySet().stream()
+                .sorted((e1, e2) -> Long.compare(e2.getValue(), e1.getValue()))
+                .limit(3).map(entry -> TopItem.newBuilder()
+                        .setName(entry.getKey())
+                        .setAmount(Math.toIntExact(entry.getValue()))
+                        .build())
+                .toList();
         TopListResponse response = TopListResponse.newBuilder().addAllTopReport(result).build();
         responseObserver.onNext(response);
         responseObserver.onCompleted();
@@ -133,9 +144,9 @@ public class StatsService extends StatsServiceGrpc.StatsServiceImplBase {
     @Override
     public void getUsersMood(UsersMoodRequest request, StreamObserver<UsersMoodResponse> responseObserver) {
         int period = periodToInt(request.getPeriod());
-        String userId = "123";
+        String userId = JWTUtils.CLIENT_ID_CONTEXT_KEY.get();
         User user = getUser(userId);
-        List<UsersMood> result = user.getMetas().stream()
+        List<UsersMood> result = getCorrectDaysSublist(user.getMetas(), request.getPeriod()).stream()
                 .map((item) -> UsersMood.newBuilder()
                         .setDate(item.getDate().toString())
                         .setScore(((Double) item.getDailyScore()).intValue()).build())
@@ -147,30 +158,11 @@ public class StatsService extends StatsServiceGrpc.StatsServiceImplBase {
 
     @Override
     public void getWeeklyReport(WeeklyReportRequest request, StreamObserver<WeeklyReportResponse> responseObserver) {
-        String id = "123";
-        List<UserDayMeta> metas = getUser(id).getMetas();
-        int size = metas.size();
-        if (size >= 7) {
-            metas = metas.subList(size - 7, size);
-        }
-        LocalDate today = LocalDate.now();
-        boolean isCorrect = false;
-        for (int i = 0; i < metas.size(); i++) {
-            if (isDateCorrect(metas.get(i).getDate(), today, periodToInt(PeriodType.WEEK))) {
-                metas = metas.subList(i, metas.size());
-                isCorrect = true;
-                break;
-            }
-        }
-        if (isCorrect) {
-            List<DayOfWeek> days = metas.stream().map(item -> DayOfWeek.forNumber(item.getDate().getDayOfWeek().getValue() - 1)).toList();
-            WeeklyReportResponse response = WeeklyReportResponse.newBuilder().addAllListOfDays(days).build();
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-        } else {
-            WeeklyReportResponse response = WeeklyReportResponse.newBuilder().build();
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-        }
+        String userId = JWTUtils.CLIENT_ID_CONTEXT_KEY.get();
+        List<UserDayMeta> metas = getCorrectDaysSublist(getUser(userId).getMetas(), PeriodType.WEEK);
+        List<DayOfWeek> days = metas.stream().map(item -> DayOfWeek.forNumber(item.getDate().getDayOfWeek().getValue() - 1)).toList();
+        WeeklyReportResponse response = WeeklyReportResponse.newBuilder().addAllListOfDays(days).build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
     }
 }
